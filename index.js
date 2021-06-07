720c0958f5e3348f4326832d20e840c242f37422
Leanne <leanne.rybintsev@gmail.com>
2021-06-07 08:23:29 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+
+const menuIcon = document.getElementById("menu-icon"); 
+const nav = document.getElementById("nav"); 
+
+    menuIcon.addEventListener("click", openMenu);
+
+function openMenu() {
+    nav.classList.toggle("toggle-nav");
+    menuIcon.classList.toggle("menu-icon-inactive")
+}
